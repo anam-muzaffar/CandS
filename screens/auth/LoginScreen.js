382b4a021a80dc33badc8cda64dc5aed84bcca1e
anam-muzaffar <44550535+anam-muzaffar@users.noreply.github.com>
2018-11-04 05:03:04 +0500

    Update LoginScreen.js

--- screens/auth/LoginScreen.js
+++ screens/auth/LoginScreen.js
@@ -1,149 +1,111 @@
 
 import React from 'react';
 import {
+  ImageBackground,
   StyleSheet,
   View,
   Text,
   TextInput,
   Button,
-  Alert,
-  ImageBackground,
-  KeyboardAvoidingView  } from 'react-native';
+  Alert } from 'react-native';
 import { NavigationActions } from 'react-navigation';
 import * as firebase from 'firebase';
 
-export default class LoginScreen extends React.Component {
+export default class SignupScreen extends React.Component {
+  static navigationOptions = {
+    header: null,
+  };
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: "",
+            password: "",
+            passwordConfirm: "",
+        };
+    }
+    onSignupPress = () => {
+        if (this.state.password !== this.state.passwordConfirm) {
+            Alert.alert("Passwords do not match");
+            return;
+        }
 
-constructor(props) {
-    super(props);
-    this.state = {
-        email: "",
-        password: "",
-    };
-}
-onLoginPress = () => {
-    firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
-        .then(() => { }, (error) => { Alert.alert(error.message); });
-}
-onCreateAccountPress = () => {
-    var navActions = NavigationActions.reset({
-        index: 0,
-        actions: [NavigationActions.navigate({routeName: "Signup"})]
-    });
-    this.props.navigation.dispatch(navActions);
-}
-onForgotPasswordPress = () => {
-    var navActions = NavigationActions.reset({
-        index: 0,
-        actions: [NavigationActions.navigate({routeName: "ForgotPassword"})]
-    });
-    this.props.navigation.dispatch(navActions);
-}
+        firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
+            .then(() => { }, (error) => { Alert.alert(error.message); });
+    }
+    onBackToLoginPress = () => {
+        var navActions = NavigationActions.reset({
+            index: 0,
+            actions: [NavigationActions.navigate({routeName: "Login"})]
+        });
+        this.props.navigation.dispatch(navActions);
+    }
 
-render() {
-    return (
-        <View style={styles.loginpage}>
-        <KeyboardAvoidingView
-          style={styles.container}
-          behavior="padding"
-          >
-          <ImageBackground source={require("../../assets/images/bckgrnd.png")} style={{width: '100%', height: '100%'}}>
+    render() {
+        return (
+            <View style={{paddingTop:50, alignItems:"center"}}>
+            <ImageBackground source={require("../../assets/images/bckgrnd.png")} style={{width: '100%', height: '100%'}}>
 
-            <Text style ={styles.title}>CandS</Text>
-            <Text style = {styles.subheading}> Crafting and Stitching</Text>
+                <Text style ={styles.title}>Crafting & Stitching</Text>
 
-            <View style={{paddingTop:50}} />
+                <TextInput style={styles.inputbox}
+                    value={this.state.email}
+                    onChangeText={(text) => { this.setState({email: text}) }}
+                    placeholder="Email"
+                    keyboardType="email-address"
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                />
 
-            <TextInput style={styles.inputbox}
-                value={this.state.email}
-                onChangeText={(text) => { this.setState({email: text}) }}
-                placeholder="Email"
-                keyboardType="email-address"
-                autoCapitalize="none"
-                autoCorrect={false}
-              />
+                <View style={{paddingTop:10}} />
 
-            <View style={{paddingTop:10}} />
+                <TextInput style={styles.inputbox}
+                    value={this.state.password}
+                    onChangeText={(text) => { this.setState({password: text}) }}
+                    placeholder="Password"
+                    secureTextEntry={true}
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                />
 
-            <TextInput style={styles.inputbox}
-                value={this.state.password}
-                onChangeText={(text) => { this.setState({password: text}) }}
-                placeholder="Password"
-                secureTextEntry={true}
-                autoCapitalize="none"
-                autoCorrect={false}
-            />
-          <View style={{paddingTop:15}} />
+                <View style={{paddingTop:10}} />
 
-            <Button
-            title="Login"
-            onPress={this.onLoginPress}
-            color="#DAA520"
-            textAlign= "center"
-            />
-
-          <View style={{paddingTop:200}} />
-
-
- <View style={styles.grid}>
-  <View style={styles.sidebox}>
-            <Button
-            title="Sign Up"
-            onPress={this.onCreateAccountPress}
-            color="#DAA520"
-          />
-  </View>
-  <View style={styles.sidebox}>
-            <Button
-             title="Forgot Password?"
-             onPress={this.onForgotPasswordPress}
-             color="black"
-              />
-  </View>
-</View>
-    </ImageBackground>
-    </KeyboardAvoidingView>
-</View>
-    );
-}
+                <TextInput
+                    style={styles.inputbox}
+                    value={this.state.passwordConfirm}
+                    onChangeText={(text) => { this.setState({passwordConfirm: text}) }}
+                    placeholder="Password (confirm)"
+                    secureTextEntry={true}
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                />
+                <View style={{paddingTop:20}} />
+                <Button
+                 title="Signup"
+                 onPress={this.onSignupPress}
+                 color="#DAA520" />
+                <View style={{paddingTop:150}} />
+                <Button
+                 title="Back to Home"
+                 onPress={this.onBackToLoginPress}
+                 color="#DAA520"  />
+            </ImageBackground>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-
-loginpage:{
- paddingTop:10,
- alignItems:"center",
- flex: 1,
-  },
-
-title: {
- paddingTop:50,
- color: "yellow",
- fontSize:50,
- textAlign:"center",
-},
-
-subheading: {
- color: "yellow",
- fontSize:20,
- textAlign:"center",
-},
-
-inputbox:{
-  width: 400,
-  height: 50,
-  borderWidth: 2,
-  borderColor: 'gray',
-  color: "#c4c633",
-  fontSize: 18
-},
-  grid: {
-        flex: 1,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    sidebox: {
-        flex: 1,
-    }
+  title: {
+   paddingTop:50,
+   color: "yellow",
+   fontSize:50,
+   textAlign:"center",
+ },
+ inputbox:{
+   width: 400,
+   height: 50,
+   borderWidth: 0,
+   color: "#c4c633",
+   fontSize: 18
+ }
 });
