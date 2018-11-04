0fc5e4a2c35e534a0d291ef5218ecfb8394adcec
anam-muzaffar <44550535+anam-muzaffar@users.noreply.github.com>
2018-11-04 05:02:07 +0500

    Update ForgotPasswordScreen.js

--- screens/auth/ForgotPasswordScreen.js
+++ screens/auth/ForgotPasswordScreen.js
@@ -1,14 +1,23 @@
 
 import React from 'react';
-import { StyleSheet, View, Text, TextInput, Button, Alert } from 'react-native';
+import {
+ImageBackground,
+StyleSheet,
+View,
+Text,
+TextInput,
+Button,
+Alert } from 'react-native';
 import { NavigationActions } from 'react-navigation';
 import * as firebase from 'firebase';
 
 export default class ForgotPasswordScreen extends React.Component {
-
+  static navigationOptions = {
+    header: null,
+  };
     constructor(props) {
         super(props);
-        this.state = { 
+        this.state = {
             email: "",
         };
     }
@@ -33,10 +42,11 @@ export default class ForgotPasswordScreen extends React.Component {
     render() {
         return (
             <View style={{paddingTop:50, alignItems:"center"}}>
+            <ImageBackground source={require("../../assets/images/bckgrnd.png")} style={{width: '100%', height: '100%'}}>
 
-                <Text>Forgot Password</Text>
-
-                <TextInput style={{width: 200, height: 40, borderWidth: 1}}
+            <Text style ={styles.title}>Crafting & Stitching</Text>
+              <View style={{paddingTop:50}} />
+                <TextInput style={styles.inputbox}
                     value={this.state.email}
                     onChangeText={(text) => { this.setState({email: text}) }}
                     placeholder="Email"
@@ -44,14 +54,35 @@ export default class ForgotPasswordScreen extends React.Component {
                     autoCapitalize="none"
                     autoCorrect={false}
                 />
-
-                <Button title="Reset Password" onPress={this.onResetPasswordPress} />
-                <Button title="Back to Login..." onPress={this.onBackToLoginPress} />
+                <View style={{paddingTop:50}} />
+                <Button
+                 title="Reset Password"
+                 onPress={this.onResetPasswordPress}
+                 color="#DAA520" />
+                <View style={{paddingTop:200}} />
+                <Button
+                 title="Back to Home"
+                 onPress={this.onBackToLoginPress}
+                 color="#DAA520" />
+                 </ImageBackground>
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
+  title: {
+   paddingTop:50,
+   color: "yellow",
+   fontSize:50,
+   textAlign:"center",
+  },
+  inputbox:{
+    width: 400,
+    height: 50,
+    borderWidth: 0,
+    color: "#c4c633",
+    fontSize: 18
+  }
 
 });
